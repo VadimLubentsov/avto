f4747b9e02a17dd8dc85ade152b3dd56f93a6a94
VadimLubentsov <153487301+VadimLubentsov@users.noreply.github.com>
2025-10-31 21:17:33 +0300

    Исправил проблемы с валидацией

--- schemas/users.py
+++ schemas/users.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel, Field, EmailStr
 class UserSchema(BaseModel):
     id: int
     login: EmailStr
-    password: str= Field(qe= 5, le=72)
+    password: str= Field()
 
     class Config:
         from_attributes = True
@@ -12,4 +12,4 @@ class UserSchema(BaseModel):
 
 class UserSchemaAdd(BaseModel):
     login: EmailStr
-    password: str=Field(qe= 5, le=72)
+    password: str=Field(min_length=5, max_length=72)
